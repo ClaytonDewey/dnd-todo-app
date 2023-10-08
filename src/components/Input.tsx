7ae90fbbbe6e24d9d2d4501102788f9dbdb5de6b
Clayton Dewey <clay@dryadmedia.com>
2023-10-08 14:41:08 -0500

    Create reusable input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,10 @@
-export const Input = () => {
-  return <input />;
-};
+import { InputHTMLAttributes, forwardRef } from "react";
+
+export const Input = forwardRef<HTMLInputElement, InputHTMLAttributes<HTMLInputElement>>(({ ...rest }, ref) => {
+  return (
+    <input
+      {...rest}
+      ref={ref}
+    />
+  )
+})
