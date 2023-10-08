fa18c9a36d4e3f04e8ef9f254500e07d35e68103
Clayton Dewey <clay@dryadmedia.com>
2023-10-08 14:41:28 -0500

    Import Input component and add submit function

--- src/components/AddToDo.tsx
+++ src/components/AddToDo.tsx
@@ -1,3 +1,45 @@
+import { useEffect, useRef, useState } from "react";
+import { useTodo } from "../hooks/useTodos";
+import { Input } from ".";
+
 export const AddToDo = () => {
-  return <div>Hello world</div>;
+  const [input, setInput] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { addTodo } = useTodo();
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  const handleSubmission = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (input.trim() !== '') {
+      addTodo(input);
+      setInput('');
+    } else {
+      // Error...
+    }
+  }
+
+  return (
+    <div className="todo todo__add">
+      <div className="todo__toggle"></div>
+      <form onSubmit={handleSubmission}>
+        <label htmlFor="todo" className="sr-only">
+          New Todo
+        </label>
+        <Input
+          ref={inputRef}
+          type='text'
+          id='todo'
+          name='todo'
+          placeholder='Create a new todo...'
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </form>
+      <div></div>
+    </div>
+  );
 };
